054a962607c718b3ca6bf531eea0cc927e05da0a
izzyzc <gamalielcruz@gmail.com>
2025-09-07 21:16:11 +0800

    Update fetch-history.js

--- scripts/fetch-history.js
+++ scripts/fetch-history.js
@@ -125,6 +125,6 @@ async function fetchHistoryForYear(year) {
 }
 
 // Run for 2022 first
-fetchHistoryForYear(2024).then(() => {
+fetchHistoryForYear(2025).then(() => {
   console.log("🎉 Done fetching 2022 draws!");
 });
